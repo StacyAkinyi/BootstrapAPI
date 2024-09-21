7daf820e6b837d9f86090edf7c5fb9f9c2d71202
StacyAkinyi <stacy.akinyi@gmail.com>
2024-09-21 20:21:26 +0300

    Edited signup form

--- forms/signup_form.php
+++ forms/signup_form.php
@@ -3,47 +3,41 @@ class signup_form{
 
 public function sign_up_form(){
         ?>
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Signup</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-        </head>
         <body>
-        <div class="container">
-            <form action="sql_signup.php" method="post"> 
-                <div class="form-group"> 
-                    <h2>Sign-Up Form</h2>
+        <div class="row align-items-md-stretch">
+            <div class="col-md-6">
+                <div class="h-100 p-5 text-bg-dark rounded-3"> 
+                    <h2>Sign Up Form</h2>
+                    <form action="<?php print basename($_SERVER["PHP_SELF"]); ?>" method="post" enctype="miltipart/form-data"> 
 
-                    <div class="signup"> 
+                    <div class="mb-3"> 
                         <label for="fullname">Full Name:</label><br>
                         <input type="text" id="fullname" name="fullname" required><br><br>
                     </div>
 
-                    <div class="signup">
+                    <div class="mb-3">
                         <label for="username">Username:</label><br>
                         <input type="text" id="username" name="username" required><br><br>
                     </div>
 
-                    <div class="signup">
+                    <div class="mb-3">
                         <label for="email">Email:</label><br>
                         <input type="email" id="email" name="email" required><br><br>
                     </div>
 
-                    <div class="signup">
+                    <div class="mb-3">
                         <label for="password">Password:</label><br> 
                         <input type="password" id="password" name="password" required><br><br>
                     </div>
 
-                    <div class="signup">
-                        <input type="submit" value="Submit">
-                    </div>
+                    <button type="submit" name="submit" class="btn btn-primary">Submit</button>	
 
-                </div> 
-            </form>  
+                
+                    </form>  
+                </div>
+            </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        
         </body>
         <?php
     }
