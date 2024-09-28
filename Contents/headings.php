66b6a73466234c404f22e9cacfbc4535ff4d2b0e
StacyAkinyi <stacy.akinyi@gmail.com>
2024-09-29 00:44:43 +0300

    Heading with bootstrap

--- Contents/headings.php
+++ Contents/headings.php
@@ -2,8 +2,11 @@
 class headings{
     public function main_banner(){
         ?>
-        <div class="banner">
-            <h1>Heading</h1>
+        <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+        <div class="container-fluid py-2">
+        <h1 class="display-5 fw-bold">Assignment II</h1>
+          <p class="col-md-12 fs-6">This is an individual assignment.</p>
+          <button class="btn btn-primary btn-lg" type="button">Example button</button>
         </div>
             <?php
     }
