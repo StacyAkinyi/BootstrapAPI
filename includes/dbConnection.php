ba18c32ed0329fa68c8a605a3c95e2621378462e
StacyAkinyi <stacy.akinyi@gmail.com>
2024-09-29 02:09:45 +0300

    dbconnection updates

--- includes/dbConnection.php
+++ includes/dbConnection.php
@@ -79,4 +79,144 @@
             }
             
         }
+        /**********************************
+ * Insert Query Method
+ *********************************/
+    public function insert($table, $data){
+        ksort($data);
+        $fieldDetails = NULL;
+        $fieldNames = implode('`, `',  array_keys($data));
+        $fieldValues = implode("', '",  array_values($data));
+        $sth = "INSERT INTO $table ($fieldNames) VALUES ('$fieldValues')";
+        return $this->extracted($sth);
     }
+/**********************************
+* Select Query From a DataBase Method
+ *********************************/
+    public function select($sql){
+        switch ($this->db_type) {
+            case 'PDO':
+                $result = $this->connection->prepare($sql);
+                $result->execute();
+                return $result->fetchAll(PDO::FETCH_ASSOC)[0];
+                break;
+            case 'MySQLi':
+                $result = $this->connection->query($sql);
+                return $result->fetch_assoc();
+                break;
+        }
+    }
+/*********************************
+* Select Query While Loop From a DataBase (tested) Method
+ *********************************/
+    public function select_while($sql){
+        switch ($this->db_type) {
+            case 'PDO':
+                $result = $this->connection->prepare($sql);
+                $result->execute();
+                return $result->fetchAll(PDO::FETCH_ASSOC);
+                break;
+            case 'MySQLi':
+                $result = $this->connection->query($sql);
+                for ($res = array (); $row = $result->fetch_assoc(); $res[] = $row);
+                return $res;
+                break;
+        }
+    }
+/**********************************
+ * Update Query (extracted) (tested) Method
+ *********************************/
+    public function update($table, $data, $where){
+        $wer = '';
+        if(is_array($where)){
+            foreach ($where as $clave=>$value){
+                $wer.= $clave."='".$value."' AND ";
+            }
+            $wer   = substr($wer, 0, -4);
+            $where = $wer;
+        }
+        ksort($data);
+        $fieldDetails = NULL;
+        foreach ($data as $key => $values){
+            $fieldDetails .= "$key='$values',";
+        }
+        $fieldDetails = rtrim($fieldDetails,',');
+        if($where==NULL or $where==''){
+            $sth = "UPDATE $table SET $fieldDetails";
+        }else {
+            $sth = "UPDATE $table SET $fieldDetails WHERE $where";
+        }
+        return $this->extracted($sth);
+    }
+/**********************************
+ * Delete Query (extracted) (tested) Method
+ *********************************/
+    public function delete($table,$where){
+        $wer = '';
+        if(is_array($where)){
+            foreach ($where as $clave=>$value){
+                $wer.= $clave."='".$value."' and ";
+            }
+            $wer   = substr($wer, 0, -4);
+            $where = $wer;
+        }
+        if($where==NULL or $where==''){
+            $sth = "DELETE FROM $table";
+        }else{
+            $sth = "DELETE FROM $table WHERE $where";
+        }
+            return $this->extracted($sth);
+    }
+/**********************************
+ * Truncate Query (extracted) Method
+ *********************************/
+    public function truncate($table){
+        $sth = "TRUNCATE $table";
+        return $this->extracted($sth);
+    }
+	
+/**********************************
+ * Get ID of Last Inserted Record Method
+ *********************************/
+	public function last_id(){
+        switch ($this->db_type) {
+        case 'PDO':
+                return $this->connection->lastInsertId();
+            break;
+		case 'MySQLi':
+			return $this->connection->insert_id;
+		break;
+		}
+	}	
+/**********************************
+ * Extracted (tested) Method
+ *********************************/
+    /**
+     * @param string $sth
+     * @return bool|string|void
+     */
+    public function extracted(string $sth)
+    {
+        switch ($this->db_type) {
+            case 'PDO':
+                try {
+                    // Prepare statement
+                    $stmt = $this->connection->prepare($sth);
+                    // execute the query
+                    $stmt->execute();
+                    return TRUE;
+                } catch (PDOException $e) {
+                    return $sth . "<br>" . $e->getMessage();
+                }
+                break;
+            case 'MySQLi':
+                if ($this->connection->query($sth) === TRUE) {
+                    return TRUE;
+                } else {
+                    return "Error: " . $sth . "<br>" . $this->connection->error;
+                }
+                break;
+        }
+    }
+}
+    
