17f9442f87254e111cf579a4e86a1fd43e4e4bb9
StacyAkinyi <stacy.akinyi@gmail.com>
2024-09-29 02:17:49 +0300

    new obj creation

--- load.php
+++ load.php
@@ -1,6 +1,11 @@
 <?php
-// class auto loader
 
+session_start();
+require "includes/constants.php";
+require "includes/dbConnection.php";
+
+
+//Class AutoLoad
 function classAutoLoad($classname){
     $directories = ["Content", "layout", "menus"];
 
@@ -14,13 +19,7 @@ function classAutoLoad($classname){
 
 spl_autoload_register('classAutoLoad');
 
-
-//require "includes/constants.php";
-//require "includes/dbConnection.php";
-
-//$conn = new dbConnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
-
-
+$ObjGlob = new fncs();
 
 //Create instance of all classes
 
@@ -28,9 +27,10 @@ $Objlayout = new layout();
 $ObjMenus = new menus();
 $Objheadings = new headings();
 $ObjContents= new contents();
+$ObjForm = new signup-form();
 $conn = new dbConnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
 
 //Create process instances
 $ObjAuth = new auth();
-$ObjAuth->signup($conn);
+$ObjAuth->signup($conn, $ObjGlob);
 ?>
